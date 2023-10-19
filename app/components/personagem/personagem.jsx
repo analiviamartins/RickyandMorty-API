bde63c1366caf295a45703999a51adfa639b1c1c
analiviamartins <anamartins.senai564@gmail.com>
2023-10-19 13:35:12 -0300

    teste

--- app/components/personagem/personagem.jsx
+++ app/components/personagem/personagem.jsx
@@ -6,8 +6,8 @@ const Personagem = ( {personagem, deletePers, atualizarCadas}) => {
     <div >
       <div className={style.card}>
         <div className={style.content} >
-          <p className={style.p}><strong>Nome: </strong>{personagem.nome}</p>
-          <p className={style.p}><strong>Estado: </strong>{personagem.estado}</p>
+          <p className={style.p}><strong>Nomes: </strong>{personagem.nome}</p>
+          <p className={style.p}><strong>Estados: </strong>{personagem.estado}</p>
           <p className={style.p}><strong>Especie: </strong>{personagem.especie}</p>
           <p className={style.p}><strong>Local: </strong>{personagem.local}</p>
           <p className={style.p}><strong>Episodio: </strong>{personagem.episodio}</p>
