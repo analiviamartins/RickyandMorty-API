d9e89b5e24ad4876d80b386599616f08fbc09d7f
Felipe Santos <dev.felipesantos@gmail.com>
2023-11-07 20:21:25 -0300

    refactor: adjust footer component path

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,19 @@
-'use client'
-import React, { useEffect, useState } from "react"
+"use client";
+import React, { useEffect, useState } from "react";
 import personagens from "@/data/charactersApi";
-import listPerso from '../models/listPerso'
-import style from '../app/page.module.css'
-import PopUp from '../app/components/PopUp/popUp';
-import Footer from '../app/components/Footer/footer';
+import listPerso from "../models/listPerso";
+import style from "./page.module.css";
+import PopUp from "./components/PopUp/popUp";
+import Footer from "./components/footer/Footer";
 import Link from "next/link";
-import Loading from "./components/loading/Loading"
+import Loading from "./components/loading/Loading";
 import Header from "./components/header/Header";
 
 const listaPersonagens = new listPerso();
-console.log(listaPersonagens)
+console.log(listaPersonagens);
 function page() {
   const [listPerso, setListaPerso] = useState([]);
-  const [dadosApi, SetDadosApi] = useState(null);
+  const [dadosApi, setDadosApi] = useState(null);
   const [escuro, setEscuro] = useState(false);
   const [flag, setFlag] = useState(0);
   const [editButton, setEditButton] = useState(false);
@@ -30,7 +30,14 @@ function page() {
   };
 
   const update = () => {
-    listaPersonagens.atualizarEdicao(flag, name, status, species, gender, image );
+    listaPersonagens.atualizarEdicao(
+      flag,
+      name,
+      status,
+      species,
+      gender,
+      image
+    );
 
     atualizarEdit();
     setEditButton(false);
@@ -48,20 +55,19 @@ function page() {
     setEstado(listaPersonagens.status);
     setEspecie(listaPersonagens.species);
     setGenero(listaPersonagens.gender);
-    setImage(listaPersonagens.image)
+    setImage(listaPersonagens.image);
   };
-
 
   useEffect(() => {
     let ignore = false;
 
     const rickmortyFetch = async () => {
       try {
-        const dados = await personagens()
+        const dados = await personagens();
         if (!ignore) {
-          SetDadosApi(dados);
+          setDadosApi(dados);
           listaPersonagens.addApiData(dados);
-          SetDadosApi(listaPersonagens.getListaPerso());
+          setDadosApi(listaPersonagens.getListaPerso());
         }
       } catch (e) {
         throw e;
@@ -72,10 +78,7 @@ function page() {
     return () => {
       ignore = true;
     };
-
   }, []);
-
-
 
   const [name, setNome] = useState("");
   const [status, setEstado] = useState("");
@@ -84,13 +87,13 @@ function page() {
   const [image, setImage] = useState("");
 
   const [showPopup, setShowPopup] = useState(false);
-  const [popupMessage, setPopupMessage] = useState('');
-  const [popupType, setPopupType] = useState('');
+  const [popupMessage, setPopupMessage] = useState("");
+  const [popupType, setPopupType] = useState("");
 
   const handleSubmit = () => {
     try {
       if (!name || !status || !species || !gender || !image) {
-        return handleShowPopup("Parâmetros incompletos", "error")
+        return handleShowPopup("Parâmetros incompletos", "error");
       }
       listaPersonagens.add(name, status, species, gender, image);
       setNome("");
@@ -98,38 +101,37 @@ function page() {
       setEspecie("");
       setGenero("");
       setImage("");
-      handleShowPopup("Cadastro concluído", "success")
+      handleShowPopup("Cadastro concluído", "success");
     } catch (error) {
       handleShowPopup("Erro aleatório", "error");
     }
 
-    console.log(handleSubmit)
+    console.log(handleSubmit);
   };
 
   const deletePers = (person) => {
     listaPersonagens.deletePers(person);
     setListaPerso(listaPersonagens.getListaPerso());
-  }
-
+  };
 
   const handleShowPopup = (message, type) => {
-    setPopupMessage(message)
-    setPopupType(type)
-    setShowPopup(true)
+    setPopupMessage(message);
+    setPopupType(type);
+    setShowPopup(true);
     setTimeout(() => {
-      setShowPopup(false)
-    }, 3000)
-  }
+      setShowPopup(false);
+    }, 3000);
+  };
 
   const tema = {
     backgroundColor: escuro ? "#1e2a39" : "#d9f7c8bc",
     color: escuro ? "#43ff2a" : "#1e2a39",
-  }
+  };
 
   const tema2 = {
     backgroundColor: escuro ? "#d9f7c8bc" : "#1e2a39",
     color: escuro ? "#1e2a39" : "#43ff2a",
-  }
+  };
 
   return (
     <>
@@ -142,53 +144,112 @@ function page() {
           <img src="/Rick-and-Morty.png" width={400} height={200} />
         </div>
         <div className={style.container}>
+          <button
+            className={`${style.button} ${style.buttons}`}
+            onClick={() => {
+              setEscuro((old) => !old);
+            }}
+          >
+            Tema
+          </button>
 
-          <button className={`${style.button} ${style.buttons}`} onClick={() => { setEscuro(old => !old) }}>Tema</button>
+          <div className={style.lista}>
+            {dadosApi ? (
+              listaPersonagens.listaPerso.map((person) => (
+                <div className={style.card} style={tema2}>
+                  <div className={style.content}>
+                    <h2 className={style.p}>{person.name}</h2>
+                    <img
+                      src={person.image}
+                      alt={person.name}
+                      width={150}
+                      height={150}
+                    />
+                    <p className={style.p}>
+                      <strong>Estado: </strong>
+                      {person.status}
+                    </p>
+                    <p className={style.p}>
+                      <strong>Especie: </strong>
+                      {person.species}
+                    </p>
+                    <p className={style.p}>
+                      <strong>Gênero: {person.gender} </strong>
+                    </p>
+                    <button
+                      className={style.remove}
+                      onClick={() => deletePers(person)}
+                    >
+                      Excluir
+                    </button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <Loading />
+            )}
+          </div>
+
           <div className={style.app} style={tema2}>
             <h1 className={style.title}>Cadastre seu personagem aqui!</h1>
-            <input value={name} className={style.input} onChange={(e) => setNome(e.target.value)} type="text" placeholder='Digite o nome' />
-            <input value={status} className={style.input} onChange={(e) => setEstado(e.target.value)} type="text" placeholder='Digite o estado (vivo, morto ...)' />
-            <input value={species} className={style.input} onChange={(e) => setEspecie(e.target.value)} type="text" placeholder='Digite a espécie' />
-            <input value={gender} className={style.input} onChange={(e) => setGenero(e.target.value)} type="text" placeholder='Digite o gênero' />
-            <input value={image} className={style.input} onChange={(e) => setImage(e.target.value)} type="text" placeholder='Link da imagem' />
-            <button className={style.button} type='button' onClick={handleSubmit}>Cadastrar</button>
-            <button className={style.button} type='button' value={editButton} onClick={() => update()}>Salvar</button>
-            <p>{showPopup && (
-              <PopUp
-                message={popupMessage}
-                type={popupType}
-              />
-            )}</p>
-          </div>
-          <div className={style.lista}>
-            {
-              dadosApi ? (
-                listaPersonagens.listaPerso.map((person) => (
-                  <div className={style.card} style={tema2}>
-                    <div className={style.content} >
-                      <h2 className={style.p}>{person.name}</h2>
-                      <img src={person.image} alt={person.name} width={150} height={150} />
-                      <p className={style.p}><strong>Estado: </strong>{person.status}</p>
-                      <p className={style.p}><strong>Especie: </strong>{person.species}</p>
-                      <p className={style.p}><strong>Gênero: {person.gender} </strong></p>
-                      <button className={style.remove} onClick={() => deletePers(person)}>Excluir</button>
-                    </div>
-
-                  </div>
-                ))
-              ) : (
-                <Loading />
-              )
-
-            }
+            <input
+              value={name}
+              className={style.input}
+              onChange={(e) => setNome(e.target.value)}
+              type="text"
+              placeholder="Digite o nome"
+            />
+            <input
+              value={status}
+              className={style.input}
+              onChange={(e) => setEstado(e.target.value)}
+              type="text"
+              placeholder="Digite o estado (vivo, morto ...)"
+            />
+            <input
+              value={species}
+              className={style.input}
+              onChange={(e) => setEspecie(e.target.value)}
+              type="text"
+              placeholder="Digite a espécie"
+            />
+            <input
+              value={gender}
+              className={style.input}
+              onChange={(e) => setGenero(e.target.value)}
+              type="text"
+              placeholder="Digite o gênero"
+            />
+            <input
+              value={image}
+              className={style.input}
+              onChange={(e) => setImage(e.target.value)}
+              type="text"
+              placeholder="Link da imagem"
+            />
+            <button
+              className={style.button}
+              type="button"
+              onClick={handleSubmit}
+            >
+              Cadastrar
+            </button>
+            <button
+              className={style.button}
+              type="button"
+              value={editButton}
+              onClick={() => update()}
+            >
+              Salvar
+            </button>
+            <p>
+              {showPopup && <PopUp message={popupMessage} type={popupType} />}
+            </p>
           </div>
         </div>
         <Footer />
       </div>
     </>
-
-
-  )
-
-};
+  );
+}
 export default page;
